51474bb06e0998b398615c3e1bbd36c63e580a97
baki504 <baki504@msn.com>
2019-06-28 20:18:30 +0900

    構造化したリファクタリング

--- src/test/java/devlove/FizzBuzzTest.java
+++ src/test/java/devlove/FizzBuzzTest.java
@@ -3,6 +3,7 @@ package devlove;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
@@ -14,28 +15,37 @@ class FizzBuzzTest {
 		fizzBuzz = new FizzBuzz();
 	}
 
-	@Test
-	void _1を渡すと文字列1に変換する() {
-		// 実行 & 検証
-		assertEquals("1", fizzBuzz.stringfy(1));
+	@Nested
+	class その他の数の時はその数を文字列に変換する {
+		@Test
+		void _1を渡すと文字列1に変換する() {
+			// 実行 & 検証
+			assertEquals("1", fizzBuzz.stringfy(1));
+		}
+
+		@Test
+		void _2を渡すと文字列2に変換する() {
+			// 実行 & 検証
+			assertEquals("2", fizzBuzz.stringfy(2));
+		}
 	}
 
-	@Test
-	void _2を渡すと文字列2に変換する() {
-		// 実行 & 検証
-		assertEquals("2", fizzBuzz.stringfy(2));
+	@Nested
+	class _3の倍数の時は数の代わりにFizzに変換する {
+		@Test
+		void _3を渡すと文字列Fizzに変換する() {
+			// 実行 & 検証
+			assertEquals("Fizz", fizzBuzz.stringfy(3));
+		}
 	}
 
-	@Test
-	void _3を渡すと文字列Fizzに変換する() {
-		// 実行 & 検証
-		assertEquals("Fizz", fizzBuzz.stringfy(3));
-	}
-
-	@Test
-	void _5を渡すと文字列Buzzに変換する() {
-		// 実行 & 検証
-		assertEquals("Buzz", fizzBuzz.stringfy(5));
+	@Nested
+	class _5の倍数の時は数の代わりにBuzzに変換する {
+		@Test
+		void _5を渡すと文字列Buzzに変換する() {
+			// 実行 & 検証
+			assertEquals("Buzz", fizzBuzz.stringfy(5));
+		}
 	}
 
 }
